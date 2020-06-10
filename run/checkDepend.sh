e4d5b40cb61213d0e8085dc1b406f17aac561fc9
Andrzej Wojciechowski <AWojciechowski93@gmail.com>
2020-06-10 23:10:50 +0200

    Updated checkDepend script to support Vivado and added colors to logs

--- run/checkDepend.sh
+++ run/checkDepend.sh
@@ -4,15 +4,19 @@
 # Copyright : Andrzej Wojciechowski (AAWO)
 # --------------------------------------------
 
-cmd_dict=( "make" "Python 3" "Icarus Verilog (iverilog)" "GTKwave" "Verilator" "cocotb" "Yosys" "Modelsim" )
-cmd_array=( "make" "python3" "iverilog" "gtkwave" "verilator" "cocotb-config" "yosys" "vsim" )
+cmd_dict=( "make" "Python 3" "Icarus Verilog (iverilog)" "GTKwave" "Verilator" "cocotb" "Yosys" "ModelSim" "Vivado" )
+cmd_array=( "make" "python3" "iverilog" "gtkwave" "verilator" "cocotb-config" "yosys" "vsim" "vivado" )
+
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
 
 for i in "${!cmd_array[@]}"
 do
    if [ -x "$(command -v ${cmd_array[$i]})" ]
    then
-      echo "OK - ${cmd_dict[$i]} is installed"
+      echo -e "${GREEN}OK${NC} - ${cmd_dict[$i]} found"
    else
-      echo "WARNING - ${cmd_dict[$i]} is not installed"
+      echo -e "${YELLOW}WARNING${NC} - ${cmd_dict[$i]} not found"
    fi
 done
